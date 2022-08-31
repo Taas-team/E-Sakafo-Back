30494d9ed513074ba184a24b0b4ada378769ca67
hei-franklin <hei.franklin.2@gmail.com>
2022-08-31 11:21:23 +0300

    Ping  / Pong

---
+++ src/main/java/mg/esakafo/taas/TaasApplication.java
@@ -0,0 +1,12 @@
+package mg.esakafo.taas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaasApplication.class, args);
+	}
+}
