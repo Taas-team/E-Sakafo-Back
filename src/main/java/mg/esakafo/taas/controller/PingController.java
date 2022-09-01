e29d17ef4f4a7413d6a6591d427434b8698f80e7
hei-franklin <hei.franklin.2@gmail.com>
2022-09-01 15:25:53 +0300

    update application.properties to heroku

--- src/main/java/mg/esakafo/taas/controller/PingController.java
+++ src/main/java/mg/esakafo/taas/controller/PingController.java
@@ -8,6 +8,11 @@ import org.springframework.web.bind.annotation.RestController;
 @CrossOrigin
 public class PingController {
 
+    @GetMapping("/")
+    public String Hello(){
+        return "Welcome to E-Kaly API";
+    }
+
     @GetMapping("/ping")
     public String ping(){
         return "Pong";
